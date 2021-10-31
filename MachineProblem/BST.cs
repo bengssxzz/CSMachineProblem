730c157e4dd3831a8d03645a67ac0cc42a991b47
Aswerty12 <biboy.janda@gmail.com>
2021-10-31 21:38:40 +0800

    Initial commit

---
+++ MachineProblem/BST.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST2
+{
+    public class Node<TKey, TValue> where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        public TKey key;
+        public TValue value;
+        public Node<TKey, TValue> left, right;
+
+        public Node(TKey key, TValue value)
+        {
+            this.key = key;
+            this.value = value;
+            left = right = null;
+        }
+
+        public string displayNode()
+        {
+            StringBuilder output = new StringBuilder();
+            displayNode(output, 0);
+            return output.ToString();
+        }
+
+        private void displayNode(StringBuilder output, int depth)
+        {
+
+            if (right != null)
+                right.displayNode(output, depth + 1);
+
+            output.Append('\t', depth);
+            output.AppendLine(value.ToString());
+
+
+            if (left != null)
+                left.displayNode(output, depth + 1);
+
+        }
+    }
+
+    public class BinarySearchTree<TKey, TValue> where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        public Node<TKey, TValue> root;
+
+        private Node<TKey, TValue> Insert(Node<TKey, TValue> root, TKey key, TValue value)
+        {
+            if (root == null)
+            {
+                root = new Node<TKey, TValue>(key, value);
+                root.key = key;
+                root.value = value;
+            }
+            else if (key.CompareTo(root.key) == -1) //less than
+                root.left = Insert(root.left, key, value);
+            else
+                root.right = Insert(root.right, key, value);
+
+            return root;
+        }
+
+        public TKey MinValue(Node<TKey, TValue> root)
+        {
+            TKey minval = root.key;
+            while (root.left != null)
+            {
+                minval = root.left.key;
+                root = root.left;
+            }
+            return minval;
+        }
+
+        private Node<TKey, TValue> Delete(Node<TKey, TValue> root, TKey key)
+        {
+            if (root == null)
+                return root;
+            else if (key.CompareTo(root.key) == -1) //less than
+                root.left = Delete(root.left, key);
+            else if (key.CompareTo(root.key) == 1)  //greater than
+                root.right = Delete(root.right, key);
+            else
+            {
+                if (root.left == null)
+                    return root.right;
+                else if (root.right == null)
+                    return root.left;
+
+                root.key = MinValue(root.right);
+                root.right = Delete(root.right, root.key);
+            }
+            return root;
+        }
+
+        private Node<TKey, TValue> Search(Node<TKey, TValue> root, TKey key)
+        {
+            if (root == null)
+                return root;
+            else if (root.key.Equals(key))
+                return root;
+            else if (key.CompareTo(root.key) == -1)
+                return Search(root.left, key);
+            else
+                return Search(root.right, key);
+        }
+
+        private void InOrder(Node<TKey, TValue> root)
+        {
+            if (root != null)
+            {
+                InOrder(root.left);
+                Console.WriteLine(root.key + " " + root.value);
+                InOrder(root.right);
+            }
+
+        }
+
+        private void PreOrder(Node<TKey, TValue> root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.key + " " + root.value);
+                PreOrder(root.left);
+                PreOrder(root.right);
+            }
+        }
+
+        public void Insert(TKey key, TValue value)
+        {
+            if (root == null)
+            {
+                root = new Node<TKey, TValue>(key, value);
+                root.key = key;
+                root.value = value;
+            }
+            else
+            {
+                Insert(root, key, value);
+            }
+        }
+
+        public void Delete(TKey key)
+        {
+            Delete(root, key);
+        }
+
+        public Node<TKey, TValue> Search(TKey key)
+        {
+            if (root.key.Equals(key))
+                return root;
+
+            else
+            {
+                return Search(root, key);
+            }
+        }
+
+        public void InOrder()
+        {
+            InOrder(root);
+        }
+
+        public void PreOrder()
+        {
+            PreOrder(root);
+        }
+
+        //    **How to use**
+        //
+        //  var tree = new BinarySearchTree<string, int>();
+        //  tree.Insert(23, "peepoo");
+        //  isFound = tree.Search(23)
+        //  if (isFound != null)
+        //      {
+        //          Console.WriteLine("{0} is found. It has a value of {1}", isFound.key, isFound.value);
+        //      }
+        //      else
+        //      {
+        //          Console.WriteLine("No such key exists.");
+        //      }
+    }
+}
---
+++ MachineProblem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using BST2;
+namespace MachineProblem
+{
+    //Kepp it object oriented, use functions and remember to comment anything that you yourself have trouble explaining.
+    class Program
+    {
+        // Standard Main Loop that lets User select Customer (Ordering) or Employee (Delivering)
+        static void Main(string[] args)
+        {
+            //Create BST and Quuue class here
+            while (true)
+            {
+                Console.WriteLine("Welcome to RESTAURANT's Delivery Service.");
+                Console.WriteLine("Are you\n1)Ordering\n2)Delivering");
+                Console.WriteLine("X)Exit");
+                string input = Console.ReadLine();
+                switch(input)
+                {
+                    case "1":
+                        Order();
+                        continue;
+                    case "2":
+                        Deliver();
+                        continue;
+                    case "X":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        continue;
+                }
+            }
+        }
+        //Menu to enter an order
+        static void Order()
+        {
+            //Create and display customer's order number (Randomly generated number not in the bst yet)
+            //Create and display current order items
+            //Provide option to add more to your order
+            //Provide option to delete from order
+            /*Only send order if it has content in string otherwise loops
+             * Then as agreed it sends the order number and order content to be INSERTED into the BST and ONLY the order number to the Queue
+             */
+            // Option to return
+        }
+        //Menu to deliver and access delivery related functions
+        static void Deliver()
+        {
+            //Option that DELIVERS, thus deleting whatever was next in queue and the associated BST entry
+            //Option that DISPLAYS ORDERS, that traverses INORDER
+            //Option that Displays next order (PEEKS at Queue but then searches and displays the associated BST entry
+            //Option to return
+        }
+    }
+}
