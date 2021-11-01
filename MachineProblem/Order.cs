be5e6d9059307cd81732273b3992c3c9d2a6d7ab
Aswerty12 <61871866+Aswerty12@users.noreply.github.com>
2021-11-01 18:53:16 +0800

    Addition of Order Class and Function (#1)
    * Order Class and Order Method in main
    
    asd
    
    * test
    
    a
    
    * Update .suo
    
    Co-authored-by: hsum4 <93267846+hsum4@users.noreply.github.com>

---
+++ MachineProblem/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineProblem
+{
+    //test
+    class Order
+    {
+        //Class attributes
+        public int orderNumber;
+        public string orderContent;
+
+        //Add function for ordering
+        public List<string> addContent(List<string> currentOrder)
+        {
+            string content;
+            Console.WriteLine("Add order content: ");
+            content = Console.ReadLine();
+
+            currentOrder.Add(content);
+
+            return currentOrder;
+        }
+
+        //Delete function
+        public List<string> deleteContent(List<string> currentOrder)
+        {
+            int contentIndex;
+            try
+            {
+                Console.WriteLine("Number of content to be deleted: ");
+                bool boolean = int.TryParse(Console.ReadLine(), out contentIndex);
+                currentOrder.RemoveAt(contentIndex);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Content does not exist");
+            }
+            
+            return currentOrder;
+        }
+    }
+}
--- MachineProblem/Program.cs
+++ MachineProblem/Program.cs
@@ -10,16 +10,18 @@ namespace MachineProblem
         static void Main(string[] args)
         {
             //Create BST and Quuue class here
+            var tree = new BinarySearchTree<int, string>();
+            Queue<int> orderQueue = new Queue<int>();
             while (true)
             {
                 Console.WriteLine("Welcome to RESTAURANT's Delivery Service.");
                 Console.WriteLine("Are you\n1)Ordering\n2)Delivering");
                 Console.WriteLine("X)Exit");
                 string input = Console.ReadLine();
-                switch(input)
+                switch (input)
                 {
                     case "1":
-                        Order();
+                        Order(tree, orderQueue);
                         continue;
                     case "2":
                         Deliver();
@@ -33,17 +35,139 @@ namespace MachineProblem
             }
         }
         //Menu to enter an order
-        static void Order()
+        static void Order(BinarySearchTree<int, string> tree, Queue<int> orderQueue)
         {
             //Create and display customer's order number (Randomly generated number not in the bst yet)
-            //Create and display current order items
-            //Provide option to add more to your order
-            //Provide option to delete from order
-            /*Only send order if it has content in string otherwise loops
-             * Then as agreed it sends the order number and order content to be INSERTED into the BST and ONLY the order number to the Queue
-             */
-            // Option to return
+            Random rnd = new Random();
+            Order new_order = new Order();
+
+            //currentOrder list for adding or deleting content later
+            List<string> currentOrder = new List<string>();
+            new_order.orderNumber = rnd.Next();
+
+            //Checks tree if orderNumber already exists
+            //if tree empty
+            if (tree.root == null)
+            {
+                //Search throws exception unless there is root
+                Console.WriteLine("root is null");
+                bool while_bool = true;
+                while (while_bool)
+                {
+                    //displays currentOrder and orderNumber
+                    Console.WriteLine("Order Number: {0}" +
+                    " | Order Content: {1}", new_order.orderNumber, string.Join(",", currentOrder));
+
+                    //choices
+                    Console.WriteLine("Press '1' to add content | Press '2' to delete content | Press '3' to return | Press '4' to continue");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            //addContent method from Order class
+                            currentOrder = new_order.addContent(currentOrder);
+                            while_bool = true;
+                            break;
+
+                        case "2":
+                            //deleteContent method from Order class
+                            currentOrder = new_order.deleteContent(currentOrder);
+                            while_bool = true;
+                            break;
+
+                        case "3":
+                            //returns to main
+                            return;
+
+                        case "4":
+                            //continues Order method
+                            while_bool = false;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+                //Converts string list currentOrder to single string then inserts and enqueues
+                new_order.orderContent = string.Join(",", currentOrder);
+                tree.Insert(new_order.orderNumber, new_order.orderContent);
+                orderQueue.Enqueue(new_order.orderNumber);
+                Console.WriteLine("Order Recieved!");
+            }
+
+            //if tree has nodes
+            else
+            {
+                //Loops until orderNumber is not in bst
+                bool while_bool = true;
+                while (while_bool)
+                {
+                    //orderNumber is in bst
+                    //search function from bst class
+                    var isFound = tree.Search(new_order.orderNumber);
+                    if (isFound != null)
+                    {
+                        Console.WriteLine("{0} is found. It has a value of {1}", isFound.key, isFound.value);
+                        new_order.orderNumber = rnd.Next();
+                        while_bool = true;
+                    }
+
+                    //orderNumber is not in bst
+                    else
+                    {
+                        bool while_bool2 = true;
+                        while (while_bool2)
+                        {
+                            //displays currentOrder and orderNumber
+                            Console.WriteLine("Order Number: {0}" +
+                            " | Order Content: {1}", new_order.orderNumber, string.Join(",", currentOrder));
+
+                            //choices
+                            Console.WriteLine("Press '1' to add content | Press '2' to delete content | Press '3' to return | Press '4' to continue");
+                            string choice = Console.ReadLine();
+                            switch (choice)
+                            {
+                                case "1":
+                                    //addContent method from Order class
+                                    currentOrder = new_order.addContent(currentOrder);
+                                    while_bool2 = true;
+                                    break;
+
+                                case "2":
+                                    //deleteContent method from Order class
+                                    currentOrder = new_order.deleteContent(currentOrder);
+                                    while_bool2 = true;
+                                    break;
+
+                                case "3":
+                                    //returns to main
+                                    return;
+
+                                case "4":
+                                    //continues Order method
+                                    while_bool2 = false;
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+
+                        //Converts string list currentOrder to single string then inserts and enqueues
+                        new_order.orderContent = string.Join(",", currentOrder);
+                        tree.Insert(new_order.orderNumber, new_order.orderContent);
+                        orderQueue.Enqueue(new_order.orderNumber);
+                        Console.WriteLine("Order Recieved!");
+
+                        while_bool = false;
+                    }
+                }
+            }
         }
+
+
+
         //Menu to deliver and access delivery related functions
         static void Deliver()
         {
@@ -53,4 +177,6 @@ namespace MachineProblem
             //Option to return
         }
     }
+
+
 }
